8c8a598b65bace6f608646bca24d84af59a6ae39
Yashraj_K <yashrajkamble7727@gmail.com>
2026-03-06 16:02:43 +0530

    UI Built

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { Box, Container, Typography } from '@mui/material'
 import './App.css'
 import useHabitStore from './store/store'
+import AddHabitForm from './compoenets/add-habit-form'
 
 function App() {
-  const store = useHabitStore()
-  console.log(store)
+  // const store = useHabitStore()
+  // console.log(store)
 
   return (
-    <>
-      <div>
-        zustand
-      </div>
-    </>
+    <Container>
+      <Box>
+        <Typography variant='h2' component="h1" gutterBottom align='center'>
+          Habit Tracker
+        </Typography>
+        <AddHabitForm/>
+        {/* form */}
+        {/* list */}
+        {/* stats */}
+      </Box>
+    </Container>
   )
 }
 
---
+++ src/compoenets/add-habit-form.tsx
@@ -0,0 +1,44 @@
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { useState } from 'react'
+
+const AddHabitForm = () => {
+    const [name, setName] = useState("");
+    const [frequency, setFrequency] = useState<"daily" | "weekly">("daily")
+
+    return (
+        <form>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: "column",
+                gap: 2
+            }}>
+                <TextField
+                    label="Habit-Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder='Enter habit name'
+                    fullWidth
+                />
+
+                <FormControl fullWidth>
+                    <InputLabel>Frequency</InputLabel>
+                    <Select
+                        value={frequency}
+                        label="frequency"
+                        onChange={(e) => setFrequency(e.target.value as "daily" | "weekly")}
+                    >
+                        <MenuItem value="daily">daily</MenuItem>
+                        <MenuItem value="weekly">weekly</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <Button type='submit' variant='contained' color='primary'>
+                    Add Habit
+                </Button>
+
+            </Box>
+        </form>
+    )
+}
+
+export default AddHabitForm
